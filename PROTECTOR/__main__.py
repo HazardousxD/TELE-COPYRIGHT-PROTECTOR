c11a33d296db8a9f76e9e44b24b90dd16576321d
Noobcoderz1 <164161601+Noobcoderz1@users.noreply.github.com>
2024-03-20 04:16:19 -0700

    Update __main__.py

--- PROTECTOR/__main__.py
+++ PROTECTOR/__main__.py
@@ -4,18 +4,17 @@ from pyrogram import idle
 from PROTECTOR import PROTECTOR
 from PROTECTOR.modules import ALL_MODULES
 
-LOGGER_ID = -1002059718978
+LOGGER_ID = -1001931272658
 
 loop = asyncio.get_event_loop()
 
-async def JARVIS():
+async def CODERZ():
     for all_module in ALL_MODULES:
         importlib.import_module("PROTECTOR.modules." + all_module)
     print("Bot Started Successfully")
     await idle()
-    print("MAI HU PIRO CODER BOLO NHI AAYA ERROR")
-    await PROTECTOR.send_message(LOGGER_ID, "**𝖨 𝖺𝗆 𝖺𝗅𝗂𝗏𝖾 𝖡𝖺𝖻𝗒 𝖸𝗈𝗎𝗋 𝖡𝗈𝗍 𝖲𝗎𝖼𝖼𝖾𝗌𝗌𝖿𝗎𝗅 𝖣𝖾𝗉𝗅𝗈𝗒 \n Mʏ Dᴇᴠᴇʟᴏᴘᴇʀ  [JARVIS](https://t.me/JARVIS_V2)**")
+    print("𝗜'𝗺 𝗡𝗼𝗼𝗯𝗱 𝗖𝗼𝗱𝗲𝗿𝘇 𝗧𝗵𝗮𝘁'𝘀 𝗪𝗵𝘆 𝗘𝗿𝗿𝗼𝗿 𝗡𝗼𝘁 𝗖𝗼𝗺𝗲 ")
+    await PROTECTOR.send_message(LOGGER_ID, "**𝗬𝗼𝘂𝗿 𝗖𝗼𝗽𝘆𝗿𝗶𝗴𝗵𝘁 𝗣𝗿𝗼𝘁𝗲𝗰𝘁𝗼𝗿 𝗕𝗼𝘁 𝗛𝗮𝘃𝗲 𝗕𝗲𝗲𝗻 𝗗𝗲𝗽𝗹𝗼𝘆𝗲𝗱 𝗦𝘂𝗰𝗰𝗲𝘀𝘀𝗳𝘂𝗹𝗹𝘆 \n Mʏ Dᴇᴠᴇʟᴏᴘᴇʀ  [𝗡𝗢𝗢𝗕 𝗖𝗢𝗗𝗘𝗥𝗭](https://t.me/noob_coderzz)**")
 
 if __name__ == "__main__":
-    loop.run_until_complete(JARVIS())
-    
+    loop.run_until_complete(CODERZ())
