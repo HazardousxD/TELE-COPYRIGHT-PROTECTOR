348aa5a71170ee78836cb95c5756cfae4b0aec6e
Noobcoderz1 <164161601+Noobcoderz1@users.noreply.github.com>
2024-03-21 00:51:22 -0700

    Update main.py

--- PROTECTOR/modules/main.py
+++ PROTECTOR/modules/main.py
@@ -34,7 +34,7 @@ async def start_command_handler(_, msg):
 
 # Callback Query Handler
 gd_buttons = [
-    [InlineKeyboardButton("𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿", url=f"https://t.me/Noob_Coderzz"),
+    [InlineKeyboardButton("𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿", url=f"https://t.me/Noob_Coderzzz"),
      InlineKeyboardButton("• ʙᴀᴄᴋ •", callback_data="back_to_start"),
      InlineKeyboardButton("𝗨𝗽𝗱𝗮𝘁𝗲𝘀", url="https://t.me/x_coderzz_bots")]
 ]
